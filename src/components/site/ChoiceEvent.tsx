fe056150b8be2d153a49f37a71335552ef9dbf28
Davi-604 <davimedeiroscv100@gmail.com>
2024-01-11 16:28:20 -0300

    adjusting a quickly deploy error

--- src/components/site/ChoiceEvent.tsx
+++ src/components/site/ChoiceEvent.tsx
@@ -3,20 +3,16 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react"
 
-export const ChoiceEvent = () => {
+type Props = {
+    onClick: (id: string) => void,
+}
+
+export const ChoiceEvent = ({onClick}: Props) => {
     const [codeInput, setCodeInput] = useState('');
-    const router = useRouter()
-
-    const searchEvent = async () => {
-        if (codeInput.trim() === '') return alert('Campo vazio, digite algum código!');
-
-        router.push(`/event/${codeInput}`)
-    };
+    
     const keyUpSearchEvent = (e: KeyboardEvent) => {
         if (e.code.toLowerCase() === 'enter') {
-            if (codeInput.trim() === '') return alert('Campo vazio, digite algum código!');
-
-            router.push(`/event/${codeInput}`)
+            onClick(codeInput)
         }
     }
 
@@ -36,7 +32,7 @@ export const ChoiceEvent = () => {
             <button
                 className="w-full py-3 border-b-4 border-r-4 mt-5 border-blue-600 bg-blue-800 text-white text-4xl rounded-md 
                 active:border-0"
-                onClick={searchEvent}
+                onClick={() => onClick(codeInput)}
             >
                 Enviar
             </button>
